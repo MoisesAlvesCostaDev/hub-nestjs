4678a9eb9f058e552b25d20dc1339459dd07b3b4
Moisés Alves Costa <moi.ac@hotmail.com>
2025-01-06 13:46:13 -0300

    feat: configuracores e instalacao de libs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGO_URI: Joi.string().required(),
+        MONGO_DB_NAME: Joi.string().required(),
+        MONGO_USER: Joi.string().required(),
+        MONGO_PASSWORD: Joi.string().required(),
+      }),
+    }),
+
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+        dbName: configService.get<string>('MONGO_DB_NAME'),
+        user: configService.get<string>('MONGO_USER'),
+        pass: configService.get<string>('MONGO_PASSWORD'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
